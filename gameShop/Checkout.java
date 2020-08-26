1e47ae7848dfc6026433f0d52fbff87fce1a335e
Jack Skiles <jackskiles420@gmail.com>
2020-08-26 17:06:56 -0400

    created a simple game shop to practice objects.

---
+++ gameShop/Checkout.java
@@ -0,0 +1,13 @@
+package gameShop;
+
+public class Checkout {
+	public static void main(String[] args) {
+		games breathOftheWild = new games();
+		
+		breathOftheWild.title = "The Legend of Zelda: Breath of the Wild";
+		breathOftheWild.company = "Nintend";
+		breathOftheWild.price = 59.99;
+		breathOftheWild.stock = 3;
+		breathOftheWild.sale = false;
+	}
+}
---
+++ gameShop/games.java
@@ -0,0 +1,9 @@
+package gameShop;
+
+public class games {
+	String title;
+	String company;
+	double price;
+	int stock;
+	boolean sale = false;
+}
