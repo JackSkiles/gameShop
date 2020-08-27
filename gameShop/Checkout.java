cf48269adedbf42f4c7d5f446ff098b2b046e3ac
Jack Skiles <jackskiles420@gmail.com>
2020-08-27 14:35:20 -0400

    updated program to check date and return sale price

--- gameShop/Checkout.java
+++ gameShop/Checkout.java
@@ -1,13 +1,40 @@
 package gameShop;
+import java.util.Calendar;
+
 
 public class Checkout {
 	public static void main(String[] args) {
 		games breathOftheWild = new games();
+		games skyrim = new games();
+		Calendar day = Calendar.getInstance();
+		Calendar saleDay = Calendar.getInstance();
+		
+		saleDay.set(Calendar.DAY_OF_MONTH, 27);
+		saleDay.set(Calendar.YEAR, 2020);
+		saleDay.set(Calendar.MONTH, 7);
+		
+		
+		skyrim.title = "Skyrim";
+		skyrim.company = "Bethesda";
+		skyrim.price = 19.99;
+		skyrim.sale = false;
+		skyrim.stock = 1;
 		
 		breathOftheWild.title = "The Legend of Zelda: Breath of the Wild";
-		breathOftheWild.company = "Nintend";
+		breathOftheWild.company = "Nintendo";
 		breathOftheWild.price = 59.99;
 		breathOftheWild.stock = 3;
 		breathOftheWild.sale = false;
+		
+		
+		
+		if(day.get(Calendar.DAY_OF_MONTH) == saleDay.get(Calendar.DAY_OF_MONTH) & day.get(Calendar.MONTH) == saleDay.get(Calendar.MONTH)) {
+			skyrim.sale = true;
+		}
+		if(skyrim.sale == true) {
+			skyrim.price = skyrim.price * .5;
+		}
+		System.out.println(skyrim.price);
+		
 	}
 }
